40ac77be9b5a8b6f26e8dc807694ee69a94bd7fa
13labragamehdi <mehdisecurite@outlook.fr>
2022-11-29 11:52:49 +0100

    Ajout d'un commentaire expliquant les variables

--- variables.py
+++ variables.py
@@ -1,3 +1,11 @@
+# l'opérateur d'affectation = permet d'injecter une valeur dans une variable
+
+my_number1 = 123
+my_number2 = -42
+
+print(my_number1)
+print(my_number2)
+
 my_text1 = "Ceci est une chaîne de caractères"
 my_text2 = "Ceci est aussi une chaîne de caracères"
 
