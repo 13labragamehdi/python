fea68b1f34a5d815c305d13ce36880d4500da7ea
13labragamehdi <mehdisecurite@outlook.fr>
2022-12-06 12:05:12 +0100

    Suite cours python

--- operators.py
+++ operators.py
@@ -5,7 +5,7 @@ foo = 123
 # + addition
 foo = 123 + 42
 foo = foo + 42
-foo += 42 (veut dire la meme chose que la ligne du dessus)
+foo += 42 #(veut dire la meme chose que la ligne du dessus)
 # / division
 foo = 123 / 42
 # // division entière
@@ -15,7 +15,7 @@ foo = 50 * 40
 # - soustraction
 foo = 123 - 42
 foo = foo - 42
-foo -= 42 ( veut dire la meme chose que la ligne du dessus)
+foo -= 42 #( veut dire la meme chose que la ligne du dessus)
 # ** puissance ( pour python sinon ^)
 foo = 2 ** 2
 foo = 2 ** 3
@@ -23,10 +23,27 @@ foo = foo ** 2
 foo = 2 ** foo
 # sqrt() racine carré
 foo = math.sqrt(4)
-ou foo = 4 ** 0.5
+foo = 4 ** 0.5
 # racine cubique
 foo = 4 ** (1/3)
 # % modulo ( reste de la division )
 foo = 4 % 3
 foo = random.randint(1,100)
 print(foo % 2)
+
+# les operateurs de comparaison
+
+# l'égalité ==
+result = 1 == 1
+print(result)
+
+# les grandeurs
+result = 123 < 42
+print(result)
+result = 123 <= 42
+result = 123 > 42
+result = 123 >= 42
+
+# inégalité
+result = 123 != 42
+print(result)
