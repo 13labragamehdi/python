41a9b78377ccdfd9c5cab530fc0c314800478b51
13labragamehdi <mehdisecurite@outlook.fr>
2022-12-12 15:21:42 +0100

    AJout exo 4

---
+++ exercices-04/exercice-04-02.py
@@ -0,0 +1,13 @@
+# exo 4.2
+# écrivez un bloc if qui affiche
+# - "le nombre est pair" si le modulo par 2 de la variable number est égal à zéro
+# - "le nombre est impair" si le modulo par 2 de la variable number n'est pas égal à zéro
+
+import random
+
+# affectaction d'un nombre entier entre 0 et 9 à la variable number
+number = random.randint(0, 9)
+print(number)
+
+# réponse 4.2
+
