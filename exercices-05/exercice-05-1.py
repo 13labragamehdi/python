6bf9088c44dd46a43f237590832994d6458c63a4
13labragamehdi <mehdisecurite@outlook.fr>
2022-12-16 11:37:42 +0100

    fin exo 5

---
+++ exercices-05/exercice-05-1.py
@@ -0,0 +1,14 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+def multiplication(a: float, b: float) -> float:
+    """Cette fonction multiplie deux nombres
+
+    a float et b float sont les nombres à multiplier
+    Return un float"""
+
+
+    return a * b
+help(multiplication)
