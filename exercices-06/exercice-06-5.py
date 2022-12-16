80d5163d1b43084516f1072959ee33750c8dc978
13labragamehdi <mehdisecurite@outlook.fr>
2022-12-16 12:36:14 +0100

    fin ex 6 - 8

---
+++ exercices-06/exercice-06-5.py
@@ -0,0 +1,8 @@
+# exo 6.5
+# Remplacez l'élément qui se trouve en deuxièm position de la liste par un nombre entier et affichez le résultat
+
+my_list = ['foo', 'bar', 'baz', 'lorem', 'ipsum']
+
+# réponse 6.5
+my_list[1] = 2
+print(my_list)
---
+++ exercices-06/exercice-06-7.py
@@ -0,0 +1,11 @@
+# exo 6.7
+# Inversez la position des valeurs `bar` et `lorem` puis affichez le résultat
+
+my_list = ['foo', 'bar', 'baz', 'lorem', 'ipsum']
+
+# réponse 6.7
+pos1 = "lorem"
+pos3 = "bar"
+my_list[1] = pos1
+my_list[3] = pos3
+print(my_list)
